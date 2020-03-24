e7c79ea5d7217a89cba7ab6d809a247cef084f69
unknown <Daniel Bennett>
2020-03-24 14:16:45 -0500

    finishing up program.cs

--- DepartmentEmployees/Program.cs
+++ DepartmentEmployees/Program.cs
@@ -16,13 +16,16 @@ namespace DepartmentsEmployees
                 /// users chooses from one of these prompts
                 Console.WriteLine("Choose from one of the options below?");
                 Console.WriteLine("--------------------------------------");
-                Console.WriteLine("1: Show all deparments");
-                Console.WriteLine("2: Show specific department by Id");
-                Console.WriteLine("3: Add new department");
-                Console.WriteLine("4: Update department Id");
-                Console.WriteLine("5: Delete department by Id");
-                Console.WriteLine("6: Show all employees");
-                Console.WriteLine("7: Show specific employee by Id");
+                Console.WriteLine("1. Show all deparments");
+                Console.WriteLine("2. Show specific department by Id");
+                Console.WriteLine("3. Add new department");
+                Console.WriteLine("4. Update department Id");
+                Console.WriteLine("5. Delete department by Id");
+                Console.WriteLine("6. Show all employees");
+                Console.WriteLine("7. Show specific employee by Id");
+                Console.WriteLine("8. Add a new employee");
+                Console.WriteLine("9. Update an employee");
+                Console.WriteLine("10. Delete an employee");
 
 
                 var choice = Console.ReadLine();
@@ -54,6 +57,63 @@ namespace DepartmentsEmployees
 
                         Console.WriteLine($"{singleDepartment.Id} {singleDepartment.DeptName}");
                         break;
+
+                    case 3:
+
+                        Console.WriteLine("Enter the name of new department.");
+                        var deptName = Console.ReadLine();
+                        Department newDepartment = new Department
+                        {
+                            DeptName = deptName
+                        };
+
+                        departments.AddDepartment(newDepartment);
+                        Console.WriteLine($"Add new {deptName} to Departments." );
+
+
+                        break;
+
+                    case 4:
+
+                        Console.WriteLine("Enter the name of the department you wish to update.");
+                        var deptName = Console.ReadLine();
+
+                        break;
+
+                    case 5:
+
+                        Console.WriteLine("Enter the Id of the deptarment you would like to delete.");
+                        var deleteChoice = int.Parse(Console.ReadeLine());
+
+                        departments.DeleteDepartment(deleteChoice);
+                        Console.WriteLine($"Department with Id of {deleteChoice} was deleted.");
+
+                        break;
+
+                    case 6:
+
+                        Console.WriteLine("Getting all employees");
+                        Console.WriteLine("");
+
+                        List<Employee> allEmployees = employees.GetAllEmployees();
+
+                        foreach (Employee e in allEmployees)
+                        {
+                            Console.WriteLine($"{e.FirstName} {e.LastName}");
+                        }
+
+                        break;
+
+                    case 7: 
+
+                        Console
+
+
+                        
+
+
+
+
 
 
 
